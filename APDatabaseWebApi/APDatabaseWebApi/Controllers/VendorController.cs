233788aa2e8f382cc14bce3e425c910aa48dbb3c
Robiul Islam Bappy <167866472+Robi14325@users.noreply.github.com>
2024-05-08 19:14:15 +0600

    Add files via upload

---
+++ APDatabaseWebApi/APDatabaseWebApi/Controllers/VendorController.cs
@@ -0,0 +1,25 @@
+﻿using APDatabaseWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace APDatabaseWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendorController : ControllerBase
+    {
+        private readonly APContext db;
+        public VendorController(APContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        public IActionResult GetVendors(string vendorCity)
+        {
+            var data = db.Vendors.FromSqlRaw($"exec AP_VendorCity_Proc @City ", new SqlParameter ("@City", vendorCity)).ToList();
+            return Ok(data);
+        }
+    }
+}
